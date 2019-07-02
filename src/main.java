f3cd51fd11839c123ffba2ac85e113b4ae57c9a4
thiplike <thip9.sp@gmail.com>
2019-07-02 13:55:57 +0700

    test pull push data

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main{
+
+     public static void main(String []args){
+        
+    	 System.out.println("Hello World");
+        
+    	 String name = "213-123-9821";
+    	 String nameSplit[] = name.split("-");
+    	
+    	 for(String n : nameSplit)
+    		 System.out.println(n);
+        
+    	 for(int i = 0; i <= 5 ; i++ )
+    		 System.out.println("MY NAME'S THIP ");
+     }
+}
